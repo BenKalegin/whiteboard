910190a8a17f1cff654f883a5f4019889e60c2ba
Ben Kalegin <BenKalegin@users.noreply.github.com>
2020-07-21 00:24:46 -0400

    Updates

---
+++ precache-manifest.a277c854cb051a115d2e464df2b5485b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "345b34b7a234a3b80867800f515afb30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6617898f230934deb4f",
+    "url": "/static/css/main.62bdde37.chunk.css"
+  },
+  {
+    "revision": "c31b3a5214aa6a1a7673",
+    "url": "/static/js/2.64ce18bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.64ce18bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6617898f230934deb4f",
+    "url": "/static/js/main.513c5000.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
