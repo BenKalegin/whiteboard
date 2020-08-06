ce3a031a4231d8561fe6bf251871e3316d01d8b5
Ben Kalegin <BenKalegin@users.noreply.github.com>
2020-08-05 21:03:14 -0400

    fixed svg colors to styles

--- webapp/src/components/Figure.tsx
+++ webapp/src/components/Figure.tsx
@@ -7,23 +7,51 @@ export interface Props {
     model: FigureModel
 }
 
-const Figure: React.FC<Props> = (props) => {
+interface StyleProps {
+    stroke: string
+    id:string
+}
+const InlineStyle: React.FC<StyleProps> = (props) => {
+    return (<style>{`
+            #${props.id}  {
+                --primary-color: ${props.stroke};
+                --secondary-color: #11EBD8;
+                --tertiary-color: #000;
+            }
+        `}
+    </style>)
+}
+const Fine: React.FC<Props> = (props) => {
     const m = props.model
-
     const translate = m.finePicture.transform.translate;
     const scale = m.finePicture.transform.scale;
+    const scaleStr: string = scale ? ` scale(${scale.x}, ${scale.y})` : ""
 
-    const translateStr = translate ? ` translate(${translate.x}, ${translate.y})` : ""
-    const scaleStr = scale ? ` scale(${scale.x}, ${scale.y})` : ""
-    const transform = `${translateStr}${scaleStr}`
+    const translateStr: string = translate? ` translate(${translate.x}, ${translate.y})` : ""
+    const transform: string = `${translateStr}${scaleStr}`
+
     return (
-        <g id={m.id} transform={transform} stroke={m.finePicture.stroke.color}>
-            {m.finePicture.name.length > 0 ?
-                findPicture(m.finePicture.name) :
-                m.curves.map((c, i) => <Curve model={c} key={i} />
-            )}
+        <g id={m.id} transform={transform}>
+            <InlineStyle stroke={m.finePicture.stroke.color} id={m.id}/>
+            {findPicture(m.finePicture.name)}
         </g>
     )
 }
+
+const Figure: React.FC<Props> = (props) => {
+    const m = props.model
+    return (
+        <React.Fragment>
+        {m.finePicture.name.length === 0 ?
+                <g id={m.id}>
+                    {m.curves.map((c, i) => <Curve model={c} key={i} />)}
+                </g>
+            : <Fine model={m}/>
+        }
+        </React.Fragment>
+    )
+}
+
+
 
 export default Figure
